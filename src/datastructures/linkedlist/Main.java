ff8d6a367f1c16e0a97c746d6274ba624601cbf2
Sandeep Wareshi <sandeep.wareshi@yahoo.co.in>
2026-04-24 22:04:22 +0530

    Linked List

---
+++ src/datastructures/linkedlist/Main.java
@@ -0,0 +1,20 @@
+package datastructures.linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+       LinkedList1 list = new LinkedList1(4);
+
+       list.getHead();
+       list.getTail();
+       list.getLength();
+       list.printList();
+       list.append(5);
+        list.getHead();
+        list.getTail();
+        list.getLength();
+        list.printList();
+
+
+
+    }
+}
